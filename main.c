c745cb79a542f7ad239bc61d51e0bc5f559b6d25
Martin-A-Silva <54644687+Martin-A-Silva@users.noreply.github.com>
2019-09-01 11:26:24 -0300

    Update main.c

--- main.c
+++ main.c
@@ -10,46 +10,122 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+#include <termios.h>
+#include <unistd.h>
+#include <string.h>
+
+
+char leeTec(int conEco) {					//Para leer caracter a caracter sin esperar '\n'
+    struct termios oldattr, newattr;
+    char ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~(conEco ? ICANON : ICANON | ECHO);
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
+
+
+int esVocal(char *a){			//Para verificar si el caracter es una vocal (retorna 1 en caso de serlo)
+
+	*a=tolower(*a);
+	int r=0;
+	char *b;
+	char str[]="aeiouáéíóú";
+
+	b=(strchr(str,*a));			//Si la función no encuentra el caracter en la cadena patrón
+								//devuelve un apuntador nulo, de lo contrario el caracter ha sido encontrado
+	if(b==NULL){
+		//Nada que hacer acá.
+	}
+	else{
+		r=1;
+	}
+
+	return r;
+
+}
+
+int finPalabra(char *a){						//Será fin de palabra cuando no sea un
+	int r=0;									//caracter del alfabeto ni una vocal acentuada
+
+	if((isalpha(*a)==0)&&(esVocal(a)==0)){
+		r=1;
+
+	}
+
+
+	return r;
+}
 
 int main(){
 
-	char str[256];
+	char *str,aux;
 	int i=0, con_vocal=0;
+	str=(char*)calloc(1,sizeof(char));					//Memoria dinámica para soportar cualquier longitud de oración
 
-	printf("Inicio del programa...\n");
-	printf("Ingrese la oración (máximo arbitrario de 256 caracteres y debe terminar con un punto):\n");
+	puts("Inicio del programa...");
+	puts("Ingrese la oración (termina al detectar un punto)");
 
-	scanf("%[^\n]",str);							//con "[^\n]" se continúa leyendo hasta que se encuentre el caracter de nueva línea.
+	while(1){
 
-	printf("La oración que ingresó es:\n%s\nY sólo se la evaluará hasta el primer punto.\n",str);
+		aux=leeTec(1);
+		if(aux=='.'){
 
-	while(*(str+i)!='.'){							//Se evalua la cadena caracter a caracter hasta que llegue al punto. Todo lo que esté después no es considerado (se supone que es sólo una oración!)
+			*(str+i)=aux;							//Se finaliza la cadena con un punto
 
-		if((*(str+i)==' ')||(*(str+i)==',')||(*(str+i)==';')){			//Se toma como separadores de palabras a los espacios, comas, y punto y comas
-			if((*(str+i-1)=='a')||(*(str+i-1)=='e')||(*(str+i-1)=='i')||	//Una vez separada la palabra, se ve si la ultima letra es una vocal
-			   (*(str+i-1)=='o')||(*(str+i-1)=='u')){
+			if(*str=='.'){
+				puts("\nNO COMENZAR CON UN PUNTO");
+				exit(1);
+			}
 
-				con_vocal++;												//Y se aumenta el contador en caso de ser así.
+			//*(str+i+1)='\0';
+			break;
+		}
 
+		char *tmp_ptr=(char *)realloc(str,(i+1)*sizeof(char));		//Se intenta aumentar el tamaño de memoria de la cadena.
+
+		if(tmp_ptr==NULL){												//Si no se logra se cierra el programa
+
+			free(str);
+			puts("ERROR DE REALLOC");
+			exit(1);
+		}
+		else{
+			str = tmp_ptr;												//Sino, se aumenta efectivamente
+		}
+
+		*(str+i)=aux;													//Y asignamos a la cadena, el caracter que se acaba de leer.
+		i++;
+	}
+
+	i=0;
+
+	while(*(str+i)!='\0'){							//Se evalua la cadena caracter a caracter hasta que llegue al caracter nulo.
+
+		if(finPalabra(str+i)){				//Cuando la función esta devuelve 1
+											//es porque se llegó al fin de una palabra.
+
+			if(i>0){						//Esta condición es para que en caso de iniciar la cadena
+											//con un símbolo, que no lo tome como fin de palabra
+				if(esVocal((str+i-1))){
+
+					con_vocal++;			//Si el caracter anterior al finalizador de
+											//palabra es vocal se aumenta el contador
+				}
 			}
 		}
 
-		i++;																//Se usa la variable i como iterador para la cadena.
+		i++;								//Se usa la variable i como iterador para la cadena.
 
 	}
 
-	if(*(str+i)=='.'){							//Esta sección es sólo
-		if((*(str+i-1)=='a')||(*(str+i-1)=='e')||(*(str+i-1)=='i')||	//para evaluar si la ÚLTIMA palabra
- 		   (*(str+i-1)=='o')||(*(str+i-1)=='u')){			//de la oración termina en vocal.
-
-			con_vocal++;
-
-		}
-
-	}
-
-	printf("La oración tiene %d palabra(s) terminada(s) en vocal.\n",con_vocal);
-	printf("Fin del programa.\n");
+	free(str);
+	printf("\nLa oración tiene %d palabra(s) terminada(s) en vocal.\n",con_vocal);
+	puts("Fin del programa.");
 	return 0;
 }
